966dc334e23e048d842aefb98052f82d6d2bb846
Benjamin Akhras <b@akhras.at>
2021-07-23 13:32:36 +0200

    initial commit

--- templates/masto_bot.py
+++ templates/masto_bot.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 
 from mastodon import Mastodon
+from os import path
+import yaml
+import click
 
-Mastodon.create_app(
-  'masto_bot',
-  api_base_url = "https://{{ server_name }}",
-  to_file = 'masto_bot.secret'
-)
 
-mastodon = Mastodon(
-    client_id = 'masto_bot.secret',
-    api_base_url = 'https://{{ server_name }}'
-)
-mastodon.log_in(
-    '{{ admin_user }}',
-    '{{ admin_password }}',
-    to_file = 'masto_bot.secret'
-)
+@click.command()
+@click.option('--user', prompt='Username: ', help='User to toot as')
+@click.option('--media', help='Media to toot')
+@click.option('--config', default='/etc/mastobot_config.yaml', help='Mastobot config')
+@click.argument('text')
+def main(user, media, config, text):
+  """Simple program that toots texts or media as user"""
+  with open(config) as f:
+    config_dict = yaml.load(f)
+  mastodon = Mastodon(
+    access_token = "masto_bot/{0}.secret".format(user),
+    api_base_url = "https://{0}".format(config_dict['server_name'])
+  )
+  if media != "" and path.isfile("{0}/{1}".format(config_dict['mediapath'], media)):
+    mastodon.media_post(media_file="{0}/{1}".format(config_dict['mediapath'], media), description=text)
+  else:
+    mastodon.toot(text)
 
-mastodon.toot('Cyber Cyber Cyber')
+if __name__ == '__main__':
+    main() # pylint: disable=no-value-for-parameter
+
